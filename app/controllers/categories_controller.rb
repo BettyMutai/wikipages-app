63ccaba5b99217338be2929ca0d049cff7f404d9
Betty Mutai <bettymutai00@gmail.com>
2017-07-31 17:16:26 +0300

    Created a table for categories and an index to display it

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,10 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+    render :index
+  end
+  def show
+    @category = Category.find(params[:id])
+    render :show
+  end
+end
