5f2a4efad34789a4657b46d6e890b93c8592ff06
Betty Mutai <bettymutai00@gmail.com>
2017-07-31 17:44:02 +0300

    Created partials for error and form to dry code and editing of category name

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,6 +19,19 @@ class CategoriesController < ApplicationController
       render :new
     end
   end
+  def edit
+    @category = Category.find(params[:id])
+    render :edit
+  end
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to categories_path
+    else
+      render :edit
+    end
+  end
+
   private
     def category_params
       params.require(:category).permit(:name)
