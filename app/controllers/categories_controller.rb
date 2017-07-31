48d5c766ae9e49ece6202f789e6c7a544c8dce74
Betty Mutai <bettymutai00@gmail.com>
2017-07-31 17:24:39 +0300

    Created a new file in categories to add new category names

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,4 +7,20 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     render :show
   end
+  def new
+    @category = Category.new
+    render :new
+  end
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
